fd5a2636e9fde1cd2b8f3ef8c15f2b351fe5fa5c
Islam Komurzoeve <jackcoul0677l@gmailr.com>
2022-03-21 16:04:46 +0300

    start back for cinema

---
+++ index.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const port = 4000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(
+    `mongodb+srv://jackcoul:1558@cluster0.rbezt.mongodb.net/NewProductsShop`
+  )
+  .then(() => {
+    console.log("Соединение с монго установлено");
+    app.listen(port, () => {
+      console.log(`http://localgost:${ports}`);
+    });
+  });
