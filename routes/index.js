97495ef590b9000e7d39964bb019f7a8a9a576dc
Islam Komurzoeve <jackcoul0677l@gmailr.com>
2022-03-22 15:22:23 +0300

    deleted modelsl and controllers and routes for genre and place

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const { Router } = require("express");
 const importFilms = require("./fims.route");
-const importGenre = require("./genre.route");
-const importPlace = require("./place.route");
 const importSeans = require("./seans.route");
 const importUser = require("./users.route");
 const importHall = require("./halls.route");
@@ -9,9 +7,7 @@ const importHall = require("./halls.route");
 const router = Router();
 
 router.use(importFilms);
-router.use(importGenre);
 router.use(importHall);
-router.use(importPlace);
 router.use(importSeans);
 router.use(importUser);
 
