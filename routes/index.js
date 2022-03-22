0e5558889bf5e97435dd2b2601a34caf99b8f11b
Islam Komurzoeve <jackcoul0677l@gmailr.com>
2022-03-22 15:54:34 +0300

    added model route and controller for busy

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const importFilms = require("./fims.route");
 const importSeans = require("./seans.route");
 const importUser = require("./users.route");
 const importHall = require("./halls.route");
+const importBusy = require("./busy.route");
 
 const router = Router();
 
 router.use(importFilms);
+router.use(importBusy);
 router.use(importHall);
 router.use(importSeans);
 router.use(importUser);
