dc6eda9a17a7138f1ef0637ebaee4308782bd2e4
Islam Komurzoeve <jackcoul0677l@gmailr.com>
2022-03-23 15:00:33 +0300

    deleted key hallNumber

--- controllers/halls.controller.js
+++ controllers/halls.controller.js
@@ -1,32 +1,29 @@
-const Hall = require("../models/Hall.model")
+const Hall = require("../models/Hall.model");
 
 module.exports.hallController = {
-    postHall: async (req,res)=>{
-        const {hallNumber,status} = req.body
-        try{
-            const hall = await Hall.create({hallNumber,status})
-            res.json(hall)
-        }
-        catch(err){
-            res.json(err.toString())
-        }
-    },
-    getHalls: async (req,res)=>{
-        try{
-            const hall = await Hall.find()
-            res.json(hall)
-        }
-        catch(err){
-            res.json(err.toString())
-        }
-    },
-    getHallById: async (req,res)=>{
-        try{
-            const hall = await Hall.findById(req.params.id)
-            res.json(hall)
-        }
-        catch(err){
-            res.json(err.toString())
-        }
+  postHall: async (req, res) => {
+    const { status } = req.body;
+    try {
+      const hall = await Hall.create({ status });
+      res.json(hall);
+    } catch (err) {
+      res.json(err.toString());
     }
-} 
+  },
+  getHalls: async (req, res) => {
+    try {
+      const hall = await Hall.find();
+      res.json(hall);
+    } catch (err) {
+      res.json(err.toString());
+    }
+  },
+  getHallById: async (req, res) => {
+    try {
+      const hall = await Hall.findById(req.params.id);
+      res.json(hall);
+    } catch (err) {
+      res.json(err.toString());
+    }
+  },
+};
